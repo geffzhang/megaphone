6c6446b3edce71aed5df5df580234b2d2066ca77
Alexandre Brisebois <brisebois@outlook.com>
2020-07-02 22:23:56 -0400

    added partitioned service

---
+++ src/Standard/Standard/Services/IPartionedStorageService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Standard.Services
+{
+    public interface IPartionedStorageService<TContent>
+    {
+        Task<TContent> GetAsync(string partitionKey, string contentKey);
+        Task SetAsync(string partitionKey, string contentKey, TContent content);
+    }
+}
